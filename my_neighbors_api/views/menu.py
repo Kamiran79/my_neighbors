077942737b7b220cc76dd7a480d23dc0eaa1f564
Kamiran Ibrahim <kamiran.ibrahim@gmail.com>
2021-03-14 11:27:08 -0500

    create Menu by Chef - CRUD

--- my_neighbors_api/views/menu.py
+++ my_neighbors_api/views/menu.py
@@ -1,4 +1,5 @@
 """View module for handling requests about game types"""
+from django.core.exceptions import ValidationError
 from my_neighbors_api.models.ingredient import Ingredient
 from my_neighbors_api.models.category import Category
 from django.db.models.base import Model
@@ -6,7 +7,7 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
-from my_neighbors_api.models import Menu
+from my_neighbors_api.models import Menu, MyNeighborsUser, Category, Ingredient
 
 class MenuView(ViewSet):
   """My Neighbors Menus"""
@@ -38,6 +39,35 @@ class MenuView(ViewSet):
         serializer = MenuViewSerializer(
             menus, many=True, context={'request': reguest})
         return Response(serializer.data)
+
+  def create(self, request):
+        chef = MyNeighborsUser.objects.get(user=request.auth.user)
+
+        menu = Menu()
+        menu.name = request.data["name"]
+        menu.ready_eat = request.data["ready_eat"]
+        menu.foodImgUrl = request.data["foodImgUrl"]
+        menu.delivery = request.data["delivery"]
+        menu.pick_up = request.data["pick_up"]
+        menu.dine_in = request.data["dine_in"]
+        menu.price = request.data["price"]
+        menu.status = request.data["status"]
+        menu.how_many_left = request.data["how_many_left"]
+
+        menu.my_neighbor_user = chef
+
+        category = Category.objects.get(pk=request.data["category"])
+        menu.category = category
+
+        try:
+            menu.save()
+            menu.ingredients.set(request.data["ingredients"])
+            menu.save()
+            serializer = MenuViewSerializer(menu, context={'request': request})
+            return Response(serializer.data)
+
+        except ValidationError as ex:
+            return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
 
 class MenuViewSerializer(serializers.ModelSerializer):
     """JSON serializer for menus
